fe23654ac0e0da9699e4cf6f08cda68413c41034
Felipe <juanfeariascastillo@live.com>
2023-02-18 13:22:42 -0500

    menu generico

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.taller1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnEmpezarJuego;
+    Button btnMostrarPaises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEmpezarJuego = (Button) findViewById(R.id.btnEmpezarJuego);
+        btnMostrarPaises = (Button) findViewById(R.id.btnMostrarPaises);
+
+        btnEmpezarJuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btnMostrarPaises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
