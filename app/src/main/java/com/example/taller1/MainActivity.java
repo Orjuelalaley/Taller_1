4f19c9b0de38e0fca1a23cc9eb4ce7cc5cdef366
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-02-19 00:12:06 -0500

    Se agregaron los iconos a los botones y la info de abajo de los mismos

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,32 +2,95 @@ package com.example.taller1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.taller1.databinding.ActivityMainBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private int contador = 0;
+    private SharedPreferences prefs;
+    private int counter_juego = 0;
+    private int counter_paises = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        prefs = getPreferences(MODE_PRIVATE);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.BtnJuego.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                contador++; // Aumentar el contador
-                actualizarTextoBoton(); // Actualizar el texto del botón
-            }
+        binding.btnEmpezarJuego.setOnClickListener(view -> {
+            counter_juego++;
+            actualizarInfoPulsado_juego();
+            guardarFecha_juego();
+        });
+        binding.btnMostrarPaises.setOnClickListener(view -> {
+            counter_paises++;
+            actualizarInfoPulsado_paises();
+            guardarFecha_paises();
         });
     }
-    private void actualizarTextoBoton() {
-        binding.Info.setText("Presionado " + contador + " veces");
+
+
+
+    private void actualizarInfoPulsado_paises() {
+        if (counter_paises  == 1)
+            binding.InfoPulsadoPaises.setText("El boton ha sido pulsado " + counter_paises + " vez.");
+        else if (counter_paises > 1){
+            binding.InfoPulsadoPaises.setText("El boton ha sido pulsado " + counter_paises + " veces.");
+        }
+    }
+
+    private void guardarFecha_juego() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault());
+        String fechaActual = dateFormat.format(new Date());
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("fecha_ultimo_click", fechaActual);
+        editor.apply();
+        mostrarFecha_juego();
+    }
+    private void guardarFecha_paises() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault());
+        String fechaActual = dateFormat.format(new Date());
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("fecha_ultimo_click", fechaActual);
+        editor.apply();
+        mostrarFecha_paises();
+    }
+
+    private void mostrarFecha_paises() {
+        String fechaGuardada = prefs.getString("fecha_ultimo_click", "Nunca");
+        binding.infoUltimaVezPaises.setText("Usado por ultima ves el  " + fechaGuardada+".");
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void mostrarFecha_juego() {
+        String fechaGuardada = prefs.getString("fecha_ultimo_click", "Nunca");
+        binding.infoUltimaVezJuego.setText("Usado por ultima ves el  " + fechaGuardada+".");
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void actualizarInfoPulsado_juego() {
+        if (counter_juego == 1)
+            binding.InfoPulsadoJuego.setText("El boton ha sido pulsado " + counter_juego + " vez.");
+        else if (counter_juego > 1){
+            binding.InfoPulsadoJuego.setText("El boton ha sido pulsado " + counter_juego + " veces.");
+        }
+    }
+
+    public void StartGame(View view) {
+    }
+
+    public void mostrarPaisses(View view) {
     }
 }
